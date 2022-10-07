1d17aca71608e2ded21ee381e70806812438b0d4
João Pedro Maria <Joaopedromaria182@gmail.com>
2022-10-07 20:31:10 -0300

    brincando

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,21 @@ public class HomeController : Controller
         return View();
     }
 
+     public IActionResult About()
+    {
+        return View();
+    }
+
+     public IActionResult Products()
+    {
+        return View();
+    }
+
+     public IActionResult Contact()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
